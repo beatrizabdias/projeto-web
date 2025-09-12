4f81fcf0316157ceb5168d2c6853a925b5a1b207
Maria Luísa <66434278+mlbcr@users.noreply.github.com>
2025-09-12 00:33:20 -0300

    Coloquei o menu mobile para maioria dos arquivos

--- js/tema.js
+++ js/tema.js
@@ -1,45 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-        const themeToggleButton = document.getElementById('theme-toggle');
-        const themeIcon = document.getElementById('theme-icon'); // Seleciona o ícone
-        const body = document.body;
+// Isso daqui espera carregar o conteúdo do html
+document.addEventListener('DOMContentLoaded', () =>{
+        const themeToggleButton = document.getElementById('theme-toggle') 
+        const themeIcon = document.getElementById('theme-icon') 
+        const body = document.body 
 
-    
-        const saveTheme = (theme) => {
-            localStorage.setItem('theme', theme);
-            updateThemeIcon(theme);
-        };
+        const saveTheme = (theme) =>{ 
+            localStorage.setItem('theme', theme)
+            updateThemeIcon(theme)
+        }; 
 
-        const updateThemeIcon = (theme) => {
+        const updateThemeIcon = (theme) =>{
             if (theme === 'light-theme') {
-                themeIcon.classList.remove('fa-sun');
-                themeIcon.classList.add('fa-moon'); 
-            } else {
-                themeIcon.classList.remove('fa-moon');
-                themeIcon.classList.add('fa-sun'); 
+                themeIcon.classList.remove('fa-sun')
+                themeIcon.classList.add('fa-moon')
+            } else{
+                themeIcon.classList.remove('fa-moon')
+                themeIcon.classList.add('fa-sun')
             }
         };
 
         const loadTheme = () => {
             const savedTheme = localStorage.getItem('theme');
-            if (savedTheme && savedTheme !== 'dark-theme') { 
-                body.classList.add(savedTheme);
-                updateThemeIcon(savedTheme); 
-            } else {
-                body.classList.remove('light-theme'); 
-                updateThemeIcon('dark-theme'); 
+            if (savedTheme && savedTheme !== 'dark-theme'){ 
+                body.classList.add(savedTheme)
+                updateThemeIcon(savedTheme)
+            } else{
+                body.classList.remove('light-theme')
+                updateThemeIcon('dark-theme')
             }
         };
 
-        themeToggleButton.addEventListener('click', (event) => {
+        themeToggleButton.addEventListener('click', (event) =>{
             event.preventDefault()
-            if (body.classList.contains('light-theme')) {
-                body.classList.remove('light-theme');
-                saveTheme('dark-theme'); 
-            } else {
-                body.classList.add('light-theme');
-                saveTheme('light-theme'); 
+            if (body.classList.contains('light-theme')){
+                body.classList.remove('light-theme')
+                saveTheme('dark-theme')
+            } else{
+                body.classList.add('light-theme')
+                saveTheme('light-theme')
             }
         });
 
-        loadTheme();
+        loadTheme()
     });
