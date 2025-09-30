36470d979327bdaf57401313dd59de172af8afdb
Beatriz <beatrizcabdias@gmail.com>
2025-09-30 12:56:10 -0300

    Implementei ArtistHeader e ArtistMaisTocadas

--- projeto-web/src/components/ArtistHeader.jsx
+++ projeto-web/src/components/ArtistHeader.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
-const ArtistHeader = () => {
-  return (
-    <div className="artist-header flex">
-      <div className="artist-info flex">
-        <h1 className="username">username</h1>
-        <p className="artist-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, inventore quisquam fuga vel...{' '}
-          <a href="#">
-            <strong>Ver mais</strong>
-          </a>
-        </p>
-        <span>10 ouvintes mensais</span>
-      </div>
-    </div>
-  );
-};
+export default function ArtistHeader() {
+    return (
+        <>
+            <div className="artist-header flex">
+                <div className="artist-info flex">
+                    <h1 className="username">username</h1>
+                    <p className="artist-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, inventore quisquam fuga vel... 
+                        <a href="#"><strong>Ver mais</strong></a>
+                    </p>
+                    <span>10 ouvintes mensais</span>
+                </div>
+            </div>
 
-export default ArtistHeader;
+            <div className="artist-actions flex">
+                <button className="play-btn">
+                    <FontAwesomeIcon icon={faPlay} />
+                </button>
+                <button className="follow-btn">
+                     Seguir
+                </button>
+            </div>
+        </>
+    );
+}
---
+++ projeto-web/src/pages/artists/Artist.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ArtistHeader from '../../components/ArtistHeader';
+import ArtistMaisTocadas from '../../components/ArtistMaisTocadas';
+
+export default function ArtistPage() {
+    return (
+        <main>
+            <ArtistHeader />
+            <ArtistMaisTocadas />
+            <Song />
+            <Song />
+            <Song />
+            <Song />
+            <Song />
+            <Song />
+         
+
+        </main>
+    );
+}
