c345eaf7903a22992a7c6722e8ed79c93dda84be
unknown <aluno@PAV1LAB1-80800.rg.cefet-rj.br>
2025-10-07 19:53:19 -0300

    Modificou a estilização do fundo

--- projeto-web/src/store/store.js
+++ projeto-web/src/store/store.js
@@ -1,11 +1,8 @@
-// src/store/store.js
-
 import { configureStore } from '@reduxjs/toolkit';
 import playerReducer from './playerSlice';
 
 export const store = configureStore({
     reducer: {
-        // Você pode ter múltiplos slices aqui (ex: user: userReducer)
         player: playerReducer,
     },
 });
